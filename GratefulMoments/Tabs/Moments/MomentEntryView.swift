f0de663c8c5485136b1a91e2259f28db0237aed9
Papa Attrams <48452451+attrams@users.noreply.github.com>
2025-12-16 05:27:59 +0000

    Add photo picker

--- GratefulMoments/Tabs/Moments/MomentEntryView.swift
+++ GratefulMoments/Tabs/Moments/MomentEntryView.swift
@@ -5,11 +5,14 @@
 //  Created by Attrams on 15/12/2025.
 //
 
+import PhotosUI
 import SwiftUI
 
 struct MomentEntryView: View {
     @State private var title = ""
     @State private var note = ""
+    @State private var imageData: Data?
+    @State private var newImage: PhotosPickerItem?
 
     var body: some View {
         NavigationStack {
@@ -18,6 +21,32 @@ struct MomentEntryView: View {
             }
             .scrollDismissesKeyboard(.interactively)
             .navigationTitle("Grateful for")
+        }
+    }
+
+    private var photoPicker: some View {
+        PhotosPicker(selection: $newImage) {
+            Group {
+                if let imageData, let uiImage = UIImage(data: imageData) {
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    Image(systemName: "photo.badge.plus.fill")
+                        .font(.largeTitle)
+                        .frame(height: 250)
+                        .frame(maxWidth: .infinity)
+                        .background(Color(white: 0.4, opacity: 0.32))
+
+                }
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 16))
+        }
+        .onChange(of: newImage) {
+            guard let newImage else { return }
+            Task {
+                imageData = try await newImage.loadTransferable(type: Data.self)
+            }
         }
     }
 
@@ -34,6 +63,8 @@ struct MomentEntryView: View {
             TextField("Log your small wins", text: $note, axis: .vertical)
                 .multilineTextAlignment(.leading)
                 .lineLimit(5...Int.max)
+
+            photoPicker
         }
         .padding()
     }
